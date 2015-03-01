8a1763eec4ff6706e0ca938ff6beef273de29855
jgrgurica <jgrgurica@extensionengine.com>
2015-02-28 23:07:32 -0500

    test

--- house_budget/settings.py
+++ house_budget/settings.py
@@ -66,8 +66,10 @@ WSGI_APPLICATION = 'house_budget.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'house_budget',
+        'USER': 'root',
+        'PASSWORD': 'hbxt34m!',
     }
 }
 
