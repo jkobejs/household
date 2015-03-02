69298d85f0afc4ab6ed61e561b52473b148402c7
jgrgurica <jgrgurica@extensionengine.com>
2015-03-01 23:30:53 -0500

    urls

--- house_budget/urls.py
+++ house_budget/urls.py
@@ -4,7 +4,7 @@ Contains urls that maps to urls of all application inside the project.
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-#from django.conf import settings
+from django.conf import settings
 
 from .views import HomeView
 from authentication.views import logout_view
@@ -17,3 +17,6 @@ urlpatterns = patterns('',
                        url(r'^logout/', view=logout_view, name="logout"),
                        url(r'^transaction/', include("transaction.urls")),
                        url(r'^graphs/', include("graphs.urls")),)
+
+urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
