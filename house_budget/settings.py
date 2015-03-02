623c283579605b458a4ec9a96ce7e776a58ecf1f
jgrgurica <jgrgurica@extensionengine.com>
2015-03-01 19:59:07 -0500

    staticfiles

--- house_budget/settings.py
+++ house_budget/settings.py
@@ -84,13 +84,14 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_PATH = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'staticfiles'
 
 STATIC_URL = '/static/'  # You may find this is already defined as such.
 
 STATICFILES_DIRS = (
-    STATIC_PATH,
+    os.path.join(BASE_DIR, 'static'),
 )
+
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 # Authentication uses
--- household_member/forms.py
+++ household_member/forms.py
@@ -6,4 +6,3 @@ class HouseholdMemberForm(HouseholdUserForm):
     def __init__(self, *args, **kwargs):
         super(HouseholdMemberForm, self).__init__(*args, **kwargs)
         self.user_type = "member"
-        
