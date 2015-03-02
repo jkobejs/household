e0d8e2439a65cae2ec92127431d32dbc6715e99f
jgrgurica <jgrgurica@extensionengine.com>
2015-03-01 20:13:11 -0500

    templates

--- house_budget/settings.py
+++ house_budget/settings.py
@@ -13,7 +13,7 @@ import os
 
 import dj_database_url
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -84,13 +84,13 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_PATH = os.path.join(BASE_DIR, 'house_budget/static')
-
-STATIC_URL = '/static/'  # You may find this is already defined as such.
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    STATIC_PATH,
+    os.path.join(BASE_DIR, 'static'),
 )
+
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 # Authentication uses
