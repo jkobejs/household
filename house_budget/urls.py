5aa53b130ecfad5a921502b2781b76b66c12ee0c
jgrgurica <jgrgurica@extensionengine.com>
2015-03-01 19:46:54 -0500

    Test

--- house_budget/urls.py
+++ house_budget/urls.py
@@ -4,7 +4,7 @@ Contains urls that maps to urls of all application inside the project.
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.conf import settings
+#from django.conf import settings
 
 from .views import HomeView
 from authentication.views import logout_view
@@ -16,4 +16,4 @@ urlpatterns = patterns('',
                        url(r'^household_manager/', include('household_manager.urls')),
                        url(r'^logout/', view=logout_view, name="logout"),
                        url(r'^transaction/', include("transaction.urls")),
-                       url(r'^graphs/', include("graphs.urls")),) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                       url(r'^graphs/', include("graphs.urls")),)
