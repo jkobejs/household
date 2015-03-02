ccd53e097becbaaa69105c0b990691cbc50461a5
jgrgurica <jgrgurica@extensionengine.com>
2015-03-01 20:03:26 -0500

    static

--- house_budget/settings.py
+++ house_budget/settings.py
@@ -84,14 +84,13 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_ROOT = 'staticfiles'
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'  # You may find this is already defined as such.
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    STATIC_PATH,
 )
-
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 # Authentication uses
