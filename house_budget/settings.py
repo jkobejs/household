e57dd7406269a3626d30c69657ff828255c65d90
jgrgurica <jgrgurica@extensionengine.com>
2015-03-01 20:08:11 -0500

    static

--- house_budget/settings.py
+++ house_budget/settings.py
@@ -84,7 +84,7 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_PATH = os.path.join(BASE_DIR, 'static')
+STATIC_PATH = os.path.join(BASE_DIR, 'house_budget/static')
 
 STATIC_URL = '/static/'  # You may find this is already defined as such.
 
